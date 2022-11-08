adf8747bba64b5c66f633e96dcd2cd7d817e2b2e
nasim0994 <nasimuddin0994@gmail.com>
2022-11-08 10:18:55 +0600

    service api create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -18,9 +18,33 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    const collection = client.db("RainbowPhotography").collection("services");
+    const serviceCollection = client
+      .db("RainbowPhotography")
+      .collection("services");
+
+    // 3 Services api for home
+    app.get("/homepageServices", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+
+    // All Services api
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // Singel Service api
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
   }
 }
 run().catch(console.dir);
